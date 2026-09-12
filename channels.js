1522d429236ea2c21f4108bb677f7a167c19f661
www-infinity4 <ephraim.nwo@gmail.com>
2026-09-12 16:57:20 -0500

    Add FOX and PBS to shared channel menu

--- channels.js
+++ channels.js
@@ -12,6 +12,8 @@
     { name: "Cartoon Network", slug: "Cartoon-Network", url: "https://www-infinity4.github.io/Cartoon-Network/" },
     { name: "WGN", slug: "WGN", url: "https://www-infinity4.github.io/WGN/" },
     { name: "NBC", slug: "NBC", url: "https://www-infinity4.github.io/NBC/" },
+    { name: "FOX", slug: "FOX", url: "https://www-infinity4.github.io/FOX/" },
+    { name: "PBS", slug: "PBS", url: "https://www-infinity4.github.io/PBS/" },
     { name: "TNT", slug: "TNT", url: "https://www-infinity4.github.io/TNT/" },
     { name: "StarQuest", slug: "TV-Database", url: "https://www-infinity4.github.io/TV-Database/" }
   ];
